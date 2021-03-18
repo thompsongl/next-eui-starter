8e790f901bdc828573fdf47bf4388cedc77e3a9a
Greg Thompson <thompson.glowe@gmail.com>
2021-03-18 14:32:30 -0500

    syntax highlighting is :thumbsup:

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import React, { FunctionComponent } from 'react';
 
-import { EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
+import { EuiSpacer, EuiCodeBlock, EuiTitle } from '@elastic/eui';
 
 const SecondPage: FunctionComponent = () => (
   <>
@@ -14,12 +14,58 @@ const SecondPage: FunctionComponent = () => (
       <h1>Second Page title</h1>
     </EuiTitle>
 
-    <EuiText>
-      <p>
-        This is just an example component that demonstrates adding pages to the
-        app.
-      </p>
-    </EuiText>
+    <EuiCodeBlock language="html">{`<!--I'm an example of HTML-->
+<div>
+  <h1>My First Heading</h1>
+  <p>My first paragraph.</p>
+</div>`}</EuiCodeBlock>
+
+    <EuiSpacer />
+
+    <EuiCodeBlock
+      language="js"
+      fontSize="m"
+      paddingSize="m"
+      overflowHeight={300}
+      isCopyable>
+      {`import React from 'react';
+
+export default () => (
+  <div className="MyComponent">
+    <h1>Hello world!</h1>
+  </div>
+);`}
+    </EuiCodeBlock>
+
+    <EuiSpacer />
+
+    <EuiCodeBlock
+      language="sql"
+      fontSize="m"
+      paddingSize="m"
+      overflowHeight={300}
+      isCopyable>
+      {`CREATE TABLE "topic" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "forum_id" integer NOT NULL,
+    "subject" varchar(255) NOT NULL
+);
+ALTER TABLE "topic"
+ADD CONSTRAINT forum_id FOREIGN KEY ("forum_id")
+REFERENCES "forum" ("id");
+
+-- Initials
+insert into "topic" ("forum_id", "subject")
+values (2, 'D''artagnian');`}
+    </EuiCodeBlock>
+
+    <EuiSpacer />
+
+    <EuiCodeBlock>
+      {`<-- Unstyled -->
+TODO
+<-- -->`}
+    </EuiCodeBlock>
 
     <EuiSpacer />
 
